e98d61d8e33a13e7a36243cf83fc22cd70c5cd0a
nwiedel <164350832+nwiedel@users.noreply.github.com>
2024-09-08 23:35:42 +0200

    Auto fährt gradeaus!

--- Racing Game/Assets/TheGame/Scripts/CarController.cs
+++ Racing Game/Assets/TheGame/Scripts/CarController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    float moveVertical, moveHorizontal;
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    float currentSpeed;
+
+    private Rigidbody2D myRb2D;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        myRb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if(moveVertical != 0)
+        {
+            currentSpeed = moveVertical * acceleration;
+
+            Vector2 movement = transform.up * currentSpeed;
+
+            myRb2D.velocity = movement;
+        }
     }
 }
